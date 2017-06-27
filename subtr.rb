601f9793bca19801347f9461749e1a89b883337b
misturfixit <japullins420@gmail.com>
2017-06-27 14:40:54 -0400

    subtr.rb

---
+++ subtr.rb
@@ -0,0 +1,15 @@
+def  subtract
+
+	print "Pick a Number??"
+	   a = gets.to_i
+	print "Now one to subtract??"
+	   b = gets.to_i
+	   c = a - b 
+	print   "#{a} Minus #{b} Equals #{c}.\n"
+
+end
+
+
+puts subtract
+
+
